cdd926c5746c3c4b4df9991ef3bc528d57ef64e0
ayuready13 <48769770+ayuready13@users.noreply.github.com>
2019-06-10 22:16:33 +0900

    create_messagemodel

--- db/migrate/20190505141847_create_maseeages.rb
+++
@@ -1,9 +0,0 @@
-class CreateMaseeages < ActiveRecord::Migration[5.0]
-  def change
-    create_table :maseeages do |t|
-      t.string      :name
-      t.text        :text
-      t.timestamps null: true
-    end
-  end
-end
