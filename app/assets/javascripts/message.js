ffe7f23698a3e075ee6eb82fb30b52e4a16e05f7
ayuready13 <48769770+ayuready13@users.noreply.github.com>
2019-07-31 22:29:14 +0900

    非同期通信追加

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,44 @@
+$(document).on('turbolinks:load', function(){
+  function buildHTML(message){
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="rightbox--center__message" data-id="${message.id}">
+                  <div class="rightbox--center__message__info">
+                    <p class="rightbox--center__message__info__name">
+                      ${message.user_name}
+                    </p>
+                    <p class="rightbox--center__message__info__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <div class="rightbox--center__message__text">
+                    <p class=rightbox--center__message__text__content">
+                      ${message.content}
+                    </p>
+                  </div>
+                  ${img}
+                </div>`
+  return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formdata = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formdata,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.rightbox--center').append(html);
+      $('#message_content').val('');
+    })
+    .fail(function(data){
+      alert('メッセージを入力してください')
+    })
+  })
+});
