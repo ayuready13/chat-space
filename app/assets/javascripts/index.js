7a3b67112ee8e1826b9ebe150f042dcffe16ec8c
ayuready13 <48769770+ayuready13@users.noreply.github.com>
2019-08-20 22:32:15 +0900

    削除追加ボタン
    うまくいかず迷走中
    削除ボタンを追加したらグループのid連動しなくなった

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,26 +1,33 @@
 $(document).on('turbolinks:load', function(){
 
   var search_list = $("#user-search-result");
-  var member_list = $("#member-append");
+  var member_list = $("#chat-group-users");
 
   function appendUser(user) {
-    var html = `
-                <div class="chat-group-user clearfix">
+    var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
-                </div>`;
-                search_list.append(html);
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                </div>`
+    search_list.append(html);
   }
 
-  function appendNoUser(msg) {
-    var html =`
-              <div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${msg}</p>
-              </div>`;
-              search_list.append(html);
+  function appendNoUser(user) {
+    var html =`<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user}</p>
+              </div>`
+    search_list.append(html);
   }
 
-  $('#user-search-field').on('keyup', function(e){
+  function appendNewUser(user_name, user_id) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='${id}' data-id='${id}'>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                    <p class='chat-group-user__name'>${name}</p>
+                    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    member_list.append(html);
+  }
+
+  $("#user-search-field").on('keyup', function(){
     var input = $("#user-search-field").val();
 
     $.ajax({
@@ -31,10 +38,10 @@ $(document).on('turbolinks:load', function(){
     })
 
     .done(function(users){
-      $('#user-search-result').empty();
-      if (users.length !== 0){
+      $("#user-search-result").empty();
+      if (users.length !== 0 && input.length !== 0){
         users.forEach(function(user){
-          appendUser(user)
+          appendUser(user);
         });
       }
       else {
@@ -47,5 +54,17 @@ $(document).on('turbolinks:load', function(){
   });
 
 
+
+  $("#user-search-result").on("click", ".chat-group-user__btn--add",function(){
+    var user_name = $(this).attr("user-name");
+    var user_id = $(this).attr("user-id");
+    var addNewUser = appendNewUser(user_name, user_id);
+    $("#chat-group-users").append(addNewUser);
+    $(this).parent('.chat-group-user').remove();
+  });
+  
+  $(document).on("click", ".user-search", function(){
+    $(this).parent().remove();
+  });
 
 });
