1fde9ae061d9c0a75dd1fbeac7e72a6228ecd246
ayuready13 <48769770+ayuready13@users.noreply.github.com>
2019-08-21 03:15:54 +0900

    追加削除ボタン迷走

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,20 +6,20 @@ $(document).on('turbolinks:load', function(){
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
     search_list.append(html);
   }
 
   function appendNoUser(user) {
     var html =`<div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${user}</p>
+                <p class="chatr-group-user__name">${user}</p>
               </div>`
     search_list.append(html);
   }
 
   function appendNewUser(user_name, user_id) {
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='${id}' data-id='${id}'>
+    var html = `<div class='chat-group-user clearfix js-chat-member' id=chat-group-user-${id}>
                   <input name='group[user_ids][]' type='hidden' value='${id}'>
                     <p class='chat-group-user__name'>${name}</p>
                     <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
